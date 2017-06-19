36eb0b293bfc6134cdd0acba0e3139c0851d8336
吴泽兵 <zebing.wu@corp.elong.com>
2017-06-19 18:18:08 +0800

    提交

---
+++ webpack.production.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+
+  entry:  __dirname + "/app/main.js",
+  output: {
+    path: __dirname + "/build",
+    filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      { test: /\.json$/, loader: "json" },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+      { test: /\.css$/, loader: 'style!css?modules!postcss' }
+    ]
+  },
+  // postcss: [
+  //   require('autoprefixer')
+  // ],
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+    })
+  ],
+
+  devServer: {
+    colors: true,
+    historyApiFallback: true,
+    inline: true
+  }
+}
